9866fda417388eed8581eb6af7ad329a9346a82f
JuanVergara-9 <juanvergara920@gmail.com>
2024-06-18 21:07:33 -0300

    Arreglos

--- app/views.py
+++ app/views.py
@@ -1,34 +1,47 @@
 from flask import render_template, flash, redirect, request, url_for
-from flask_login import current_user, login_user, logout_user, login_required
+from flask_login import current_user, login_user, logout_user, login_required, UserMixin
 from urllib.parse import urlsplit  # Importación corregida
 from flask import Blueprint
 
 main_bp = Blueprint('main_bp', __name__)
 
+valid_table_numbers = {1, 2, 3, 4, 5}
+
+class User(UserMixin):
+    def __init__(self, id, name, table_number):
+        self.id = id
+        self.name = name
+        self.table_number = table_number
+
+    @staticmethod
+    def get(user_id):
+
+        return User(user_id, "Placeholder", user_id)
+
 @main_bp.route('/')
 @main_bp.route('/index')
 @login_required
 def index():
-    return render_template('index.html', title='Home')
+    return render_template('index.html', title='Home', name=current_user.name, table_number=current_user.table_number)
 
 @main_bp.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('main_bp.index'))
-    from app.forms import LoginForm
-    form = LoginForm()
-    if form.validate_on_submit():
-        from app.models import User
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
+    if request.method == 'POST':
+        name = request.form['name']
+        table_number = int(request.form['table_number'])
+        if table_number in valid_table_numbers:
+            user = User(table_number, name, table_number)
+            login_user(user)
+            next_page = request.args.get('next')
+            if not next_page or urlsplit(next_page).netloc != '': 
+                next_page = url_for('main_bp.index')
+            return redirect(next_page)
+        else:
+            flash('Número de mesa inválido')
             return redirect(url_for('main_bp.login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or urlsplit(next_page).netloc != '':  # Uso de urlsplit
-            next_page = url_for('main_bp.index')
-        return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html', title='Ingreso Cliente')
 
 @main_bp.route('/logout')
 def logout():
